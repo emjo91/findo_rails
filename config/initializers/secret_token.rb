af4a5ac78fc818b414e733a3f1ad1be7830e8309
EmilyJohnson <3mle33@gmail.com>
2015-03-20 13:18:01 -0500

    Created Findo Rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FindoRails::Application.config.secret_token = 'a64ea792c4c14d86235784caa944fe6692368d25bb418afafa0e2f11f78f31232f3d52381deacb9c14ecbda2cd1b91f665f19f674960daf9d1bdcd324dfda4a3'
